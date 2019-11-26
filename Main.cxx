7838e49d50b64b33e40192e25b45909c8e702c78
maoshenggao <55608152+maoshenggao@users.noreply.github.com>
2019-11-25 21:21:23 -0500

    上传文件

---
+++ Main.cxx
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "HelloWorld.hxx"
+using namespace std;
+
+int main()
+{
+     cout << HelloWorld() << endl;
+     return 0;
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+g++ -g -g HelloWorld.o -c HelloWorld.cxx
+g++ -g -o Main.o -c Main.cxx
+g++ -g -o HelloWorld.exe HelloWorld.o Main.o
